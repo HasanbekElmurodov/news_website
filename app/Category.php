7acf4a1556c006d48886ee5cc75c5bf1c2f75e75
Hasanbek Elmurodov <hasanbek.elmurodov@gmail.com>
2021-02-19 19:43:07 +0500

    initial commit

---
+++ app/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ['user_id', 'thumbnail', 'name', 'slug', 'is_published'];
+
+    public function user()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
